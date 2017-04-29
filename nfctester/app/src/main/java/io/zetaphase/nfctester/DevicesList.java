1b8840cc32688d89f31366aad4b3b9357e52a3c7
xeliot <daveho8888@gmail.com>
2017-04-28 21:50:39 -0700

    DevicesList is new activity

--- nfctester/app/src/main/java/io/zetaphase/nfctester/DevicesList.java
+++ nfctester/app/src/main/java/io/zetaphase/nfctester/DevicesList.java
@@ -1,8 +1,10 @@
 package io.zetaphase.nfctester;
 
+import android.app.Activity;
+
 /**
  * Created by xeliot on 4/28/17.
  */
 
-public class DevicesList {
+public class DevicesList extends Activity{
 }
