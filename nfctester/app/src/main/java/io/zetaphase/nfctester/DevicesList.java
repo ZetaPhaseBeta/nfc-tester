8b4cf99efc547c2b96b117a80d352dbd4b05b1c5
xeliot <daveho8888@gmail.com>
2017-04-29 21:01:15 -0700

    change contentview layout for device list activity

--- nfctester/app/src/main/java/io/zetaphase/nfctester/DevicesList.java
+++ nfctester/app/src/main/java/io/zetaphase/nfctester/DevicesList.java
@@ -35,7 +35,7 @@ public class DevicesList extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.connected_nfc_devices);
 
         mTextView = (TextView) findViewById(R.id.Textview1);
 
