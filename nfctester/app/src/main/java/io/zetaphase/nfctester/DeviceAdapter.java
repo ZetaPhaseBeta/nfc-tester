481e8f2eafaa03fa7dcd21601979f317fa9cb400
xeliot <daveho8888@gmail.com>
2017-04-30 20:28:20 -0700

    create device class

--- nfctester/app/src/main/java/io/zetaphase/nfctester/DeviceAdapter.java
+++ nfctester/app/src/main/java/io/zetaphase/nfctester/DeviceAdapter.java
@@ -1,8 +1,10 @@
 package io.zetaphase.nfctester;
 
+import android.widget.ArrayAdapter;
+
 /**
  * Created by xeliot on 4/30/17.
  */
 
-public class DeviceAdapter {
+public class DeviceAdapter extends ArrayAdapter<Device>{
 }
