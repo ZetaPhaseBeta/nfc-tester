e40e528206b39b654f2368b799eb6013a2d9ba5a
xeliot <daveho8888@gmail.com>
2017-04-30 20:32:22 -0700

    finish basic skeleton of device class

--- nfctester/app/src/main/java/io/zetaphase/nfctester/Device.java
+++ nfctester/app/src/main/java/io/zetaphase/nfctester/Device.java
@@ -5,4 +5,21 @@ package io.zetaphase.nfctester;
  */
 
 public class Device {
+    private String name;
+
+    public Device(String name){
+        this.name = name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String toString(){
+        return this.name;
+    }
 }
