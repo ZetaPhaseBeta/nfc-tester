04328955f59b97705acda215b08ac34ccb072387
xeliot <daveho8888@gmail.com>
2017-05-03 17:34:25 -0700

    finish splash screen class

--- nfctester/app/src/main/java/io/zetaphase/nfctester/SplashScreen.java
+++ nfctester/app/src/main/java/io/zetaphase/nfctester/SplashScreen.java
@@ -1,7 +1,9 @@
 package io.zetaphase.nfctester;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 /**
  * Created by xeliot on 5/3/17.
@@ -12,6 +14,16 @@ public class SplashScreen extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
-        
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, DevicesList.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
