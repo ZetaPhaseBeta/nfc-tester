9f929b79af86412edabd3bc592a0d807bfc90917
xeliot <daveho8888@gmail.com>
2017-05-03 17:32:05 -0700

    initialize splash screen

--- nfctester/app/src/main/java/io/zetaphase/nfctester/SplashScreen.java
+++ nfctester/app/src/main/java/io/zetaphase/nfctester/SplashScreen.java
@@ -1,8 +1,17 @@
 package io.zetaphase.nfctester;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by xeliot on 5/3/17.
  */
 
-public class SplashScreen {
+public class SplashScreen extends Activity{
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        
+    }
 }
