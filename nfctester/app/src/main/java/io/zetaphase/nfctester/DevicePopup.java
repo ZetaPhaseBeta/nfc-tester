1d102084b1b49b67aea732881765cb89e781bcaf
xeliot <daveho8888@gmail.com>
2017-05-08 20:59:12 -0700

    create constructor

--- nfctester/app/src/main/java/io/zetaphase/nfctester/DevicePopup.java
+++ nfctester/app/src/main/java/io/zetaphase/nfctester/DevicePopup.java
@@ -1,10 +1,16 @@
 package io.zetaphase.nfctester;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by xeliot on 5/3/17.
  */
 
 public class DevicePopup extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        
+    }
 }
