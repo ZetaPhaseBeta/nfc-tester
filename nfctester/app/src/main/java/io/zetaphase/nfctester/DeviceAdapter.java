657a2b1f536627b845911e6491fa8eb73b4e5b29
xeliot <daveho8888@gmail.com>
2017-11-27 20:53:54 -0800

    need to get difference of time

--- nfctester/app/src/main/java/io/zetaphase/nfctester/DeviceAdapter.java
+++ nfctester/app/src/main/java/io/zetaphase/nfctester/DeviceAdapter.java
@@ -34,6 +34,7 @@ public class DeviceAdapter extends ArrayAdapter<Device>{
         TextView duration = (TextView) view.findViewById(R.id.device_row_duration);
 
         name.setText(device.getName());
+        // time connected = current time - device.getTimeConnected() [update this every period of time]
         duration.setText(Float.toString(device.getTimeConnected()));
 
         return view;
